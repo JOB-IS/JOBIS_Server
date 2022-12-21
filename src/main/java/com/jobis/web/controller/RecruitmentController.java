2ee113864c425cfc7c71ce0b968b43fc5f0976cf
huey-J <wkdgus7113@gmail.com>
2022-12-21 20:21:31 +0900

    recruitment 수정 API 추가

--- src/main/java/com/jobis/web/controller/RecruitmentController.java
+++ src/main/java/com/jobis/web/controller/RecruitmentController.java
@@ -3,6 +3,7 @@ package com.jobis.web.controller;
 import com.jobis.config.security.LoginUser;
 import com.jobis.web.dto.request.recruitment.CreateRecruitmentRequestDTO;
 import com.jobis.web.dto.request.recruitment.RecruitmentSearchRequestDTO;
+import com.jobis.web.dto.request.recruitment.UpdateRecruitmentRequestDTO;
 import com.jobis.web.dto.response.RecruitmentResponseDTO;
 import com.jobis.web.service.RecruitmentService;
 import javax.validation.Valid;
@@ -12,7 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +31,7 @@ public class RecruitmentController {
   public ResponseEntity<Page<RecruitmentResponseDTO>> getRecruitments(
       @AuthenticationPrincipal LoginUser loginUser, @Valid RecruitmentSearchRequestDTO dto) {
     Page<RecruitmentResponseDTO> responseDTO = recruitmentService
-        .getRecruitments(dto, loginUser.getUserId());
+        .getRecruitments(loginUser.getUserId(), dto);
     return ResponseEntity.ok(responseDTO);
   }
 
@@ -37,7 +40,17 @@ public class RecruitmentController {
       @AuthenticationPrincipal LoginUser loginUser,
       @Valid @RequestBody CreateRecruitmentRequestDTO dto) {
     RecruitmentResponseDTO responseDTO = recruitmentService
-        .createRecruitment(dto, loginUser.getUserId());
+        .createRecruitment(loginUser.getUserId(), dto);
+    return ResponseEntity.ok(responseDTO);
+  }
+
+  @PutMapping(value = "/{recruitment-id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Boolean> updateRecruitment(
+      @AuthenticationPrincipal LoginUser loginUser,
+      @PathVariable("recruitment-id") long recruitmentId,
+      @RequestBody UpdateRecruitmentRequestDTO dto) {
+    boolean responseDTO = recruitmentService
+        .updateRecruitment(loginUser.getUserId(), recruitmentId, dto);
     return ResponseEntity.ok(responseDTO);
   }
 
